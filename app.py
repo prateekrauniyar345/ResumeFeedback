bbbbad5c87e436bc8180a03581ce033f32c1f45e
prateekrauniyar345 <raun9583@vandals.uidaho.edu>
2025-08-16 01:35:15 -0700

    added the header and other html files

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
 
 load_dotenv()
 
 
-app = Flask(__name__)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 @app.route('/', methods=['GET'])
 def main():
-    return "Hello, World!"
+    return render_template('dashboard/root.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('login.html')
 
 
 
